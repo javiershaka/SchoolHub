06b8888dafdecebeea902e0092203503aff9dfc6
javiershaka <32804342+javiershaka@users.noreply.github.com>
2017-12-05 21:39:30 -0700

    Add files via upload

---
+++ SchoolHub.Nucleo/src/mx/itson/SchoolHub/enumeradores/TiempoEngrega.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.SchoolHub.enumeradores;
+
+/**
+ *
+ * @author javiershaka
+ */
+public enum TiempoEngrega {
+    ATiemp,Retraso,NoEntregado;
+}
